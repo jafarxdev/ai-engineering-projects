5c31637e5c3764f2292e1700e0451c5ff19bb616
Jafar Madadi <jafar.madadi@afghantelecom.af>
2026-09-08 15:56:54 +0430

    feat: add basic chat bot response loop which do not respond to user dynamically yet

--- chat_bot.py
+++ chat_bot.py
@@ -1,8 +1,12 @@
+def bot(message):
+    answer = "I'm not sure how to respond to that."
+    return answer
 
 
 def main():
     user_input = input("Ask me anything: ")
-    print(f"You asked: {user_input}")
+    bot_response = bot(user_input)
+    print(bot_response)
 
 
 if __name__ == "__main__":
